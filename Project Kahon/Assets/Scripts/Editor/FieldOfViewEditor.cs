06065b273995aedcc386a93ca02ac0fa1c31aee6
danielconde001 <danielconde001@yahoo.com>
2021-03-21 17:40:07 +0800

    Added Player Detection + Field of View

---
+++ Project Kahon/Assets/Scripts/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    private void OnSceneGUI() {
+        FieldOfView FOV = target as FieldOfView;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(FOV.transform.position, Vector3.up,Vector3.forward, 360f, FOV.ViewRadius);
+
+        Vector3 angleA = FOV.DirectionFromAngle(-FOV.ViewAngle / 2, false);
+        Vector3 angleB = FOV.DirectionFromAngle(FOV.ViewAngle / 2, false);
+
+        Handles.DrawLine(FOV.transform.position, FOV.transform.position + (angleA * FOV.ViewRadius));
+        Handles.DrawLine(FOV.transform.position, FOV.transform.position + (angleB * FOV.ViewRadius));
+    }
+}
